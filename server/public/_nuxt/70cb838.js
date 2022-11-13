1b73178daf5b2162a70bae88d11e5f13d741800b
softBelle <younglady19931114@gmail.com>
2022-11-13 19:10:35 +0600

    public updated

--- server/public/_nuxt/70cb838.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{404:function(n,t,e){"use strict";e.r(t);var l=e(35),component=Object(l.a)({},(function(){return(0,this._self._c)("div",[this._v("\n    Hello, Data Manager\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
