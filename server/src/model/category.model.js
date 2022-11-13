1c216aa5d50b4a24234a3e4091077a7b6d8e7fbc
Tan Zhi Xuan <69501009+Zhixuan0318@users.noreply.github.com>
2022-11-13 20:36:43 +0800

    Delete server directory

--- server/src/model/category.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const category = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const Category = mongoose.model('Category', category);
-
-module.exports = Category;
